e99f3ba7e5467a0720754e3d6ef97b99974aec68
Roi Romem <real.roiromem@gmail.com>
2024-09-03 23:20:42 +0300

    added parts of the game ig

--- Scuffed ghost fighting/Assets/scripts/EnemyPatrolAI.cs
+++ Scuffed ghost fighting/Assets/scripts/EnemyPatrolAI.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class EnemyPatrolAI : MonoBehaviour
 {
-    private Transform Player;
-    public float moveSpeed = 1f;
+    private GameObject Player;
+    public float moveSpeed = 0.5f;
+    int MaxDist = 0;
+    int MinDist = 5;    
 
     // Start is called before the first frame update
     void Awake()
     {
-        Player = GameObject.Find("Player").transform;
+        Player = GameObject.Find("Player");
     }
 
     
     void FixedUpdate()
     {
-        transform.LookAt(Player);
-        transform.position += transform.forward * moveSpeed;
+        transform.LookAt(Player.transform);
+
+        if (Vector3.Distance(transform.position, Player.transform.position) >= MinDist)
+        {
+            transform.position += transform.forward * moveSpeed;
+        } 
+        else
+        {
+            //deal damage to player
+        }
     }
 }
